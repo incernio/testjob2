334fba40ab59658ae7700cde1a794735c4574c2c
Incenrio <incernio@gmail.com>
2016-02-05 21:12:36 +0300

    project base files

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=testjob2',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'tablePrefix' => 'tj2_',
+];
